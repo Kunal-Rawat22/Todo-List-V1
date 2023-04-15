40b5505ca7d40399c4c9618fd7072bee768db6fc
Kunal-Rawat22 <Kunalrawat399@gmail.com>
2023-04-15 14:01:30 +0530

    Creating EJS File

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     let today = new Date();
     let currentDay = today.getDay();
