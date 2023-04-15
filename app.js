60d55e096c1dcac8c850b0384ebbc2f5d8a0d5e7
Kunal-Rawat22 <Kunalrawat399@gmail.com>
2023-04-16 01:01:08 +0530

    Addable Items

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+let items = [];
 
 app.get('/', (req, res) => {
     let today = new Date();
     let currentDay = today.getDay();
-
-    if (currentDay===6 || currentDay===0)
-        res.write("<h1>Yay Today is Weekend !!</h1>");
-    else
-        res.write("<h1>Boo, I ahve to work !!</h1>");
-    // res.send("Hi Just Checking");
+    let option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    }
+    let day = today.toLocaleDateString("en-US", option);
+    console.log(day);
+    // if (currentDay === 0)
+    //     day = "Sunday";
+    // else if (currentDay === 1)
+    //     day = "Monday";
+    // else if (currentDay === 2)
+    //     day = "Tuesday";
+    // else if (currentDay === 3)
+    //     day = "Wednesday";
+    // else if (currentDay === 4)
+    //     day = "Thursday";
+    // else if (currentDay === 5)
+    //     day = "Friday";
+    // else
+    //     day = "Saturday";
+    
+    res.render("list2", { kindOfDay : day, listOfItems: items });
     res.send();
 })
-
+app.post('/', function (req, res) {
+    let todoitem = req.body.todoitem;
+    items.push(todoitem);
+    res.redirect('/');
+  })
 app.listen(3000, (req, res) => {
     console.log("Server Running in Post 3000");
 }
